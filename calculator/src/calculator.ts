675b341393f8b61ca39ed35f5548978d30a47ec4
jaenya <jaenya@xnsystems.com>
2017-02-17 11:03:22 +0900

    systemjs

---
+++ calculator/src/calculator.ts
@@ -0,0 +1,10 @@
+function calculate(num) {
+    calculator.screen.value += num;
+}
+function equals() {
+    calculator.screen.value = eval(document.calculator.screen.value);
+}
+function clean() {
+    calculator.screen.value = "";
+}
+console.log("OK");
